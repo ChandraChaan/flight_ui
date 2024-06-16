29dfb587bc79227674931578f9127c4d1296dde4
chandraobulareddy <dchandrachaan@gmail.com>
2024-06-16 23:07:58 +0530

    UI modification

--- lib/ticket_booking.dart
+++ lib/ticket_booking.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 import 'enums.dart';
 
@@ -23,7 +26,27 @@ class _FlightSeatBookingScreenState extends State<FlightSeatBookingScreen> {
           Column(
             children: <Widget>[
               _buildHeader(),
-              Expanded(child: _buildSeatMap()),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      ClipPath(
+                        clipper: AirplaneNoseClipper(),
+                        child: Container(
+                          height: 400,
+                          width: MediaQuery.of(context).size.width / 1.5,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Container(
+                        height: MediaQuery.of(context).size.height * 2,
+                        width: MediaQuery.of(context).size.width / 1.5,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
               _buildContinueButton(),
             ],
           ),
@@ -36,12 +59,15 @@ class _FlightSeatBookingScreenState extends State<FlightSeatBookingScreen> {
     return Container(
       decoration: const BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('assets/images/flight_background.jpeg',),
+          image: AssetImage(
+            'assets/images/flight_background.jpeg',
+          ),
           fit: BoxFit.cover,
         ),
       ),
     );
   }
+
   Widget _buildHeader() {
     return Container(
       padding: const EdgeInsets.all(16),
@@ -51,48 +77,78 @@ class _FlightSeatBookingScreenState extends State<FlightSeatBookingScreen> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Icon(Icons.arrow_back_ios_new, color: Colors.white,),
-                Icon(Icons.edit_note,color: Colors.white,),
+                Icon(
+                  Icons.arrow_back_ios_new,
+                  color: Colors.white,
+                ),
+                Icon(
+                  Icons.edit_note,
+                  color: Colors.white,
+                ),
               ],
             ),
             Text(
               '20 June 2024',
-              style: TextStyle(fontSize: 18,color: Colors.white,),
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
             ),
-            Row( mainAxisAlignment: MainAxisAlignment.center,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
                   'DEL',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold,color: Colors.white,),
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
                 Text(
                   '-----',
-                  style: TextStyle( color: Colors.orange),
+                  style: TextStyle(color: Colors.orange),
                 ),
-                Icon(Icons.flight_land, color: Colors.orange,),
+                Icon(
+                  Icons.flight_land,
+                  color: Colors.orange,
+                ),
                 Text(
                   '-----',
-                  style: TextStyle( color: Colors.orange),
+                  style: TextStyle(color: Colors.orange),
                 ),
                 Text(
                   'UAE',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold,color: Colors.white,),
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
               ],
             ),
             SizedBox(height: 16),
             Text(
               '03h 55m',
-              style: TextStyle(fontSize: 18,color: Colors.white,),
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
             ),
             SizedBox(height: 6),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(Icons.person,color: Colors.white,),
+                Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
                 Text(
                   '1 Adult',
-                  style: TextStyle(fontSize: 18,color: Colors.white,),
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
                 ),
               ],
             ),
@@ -178,5 +234,24 @@ class _FlightSeatBookingScreenState extends State<FlightSeatBookingScreen> {
         child: const Text('Continue'),
       ),
     );
+  }
+}
+
+class AirplaneNoseClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.moveTo(0, size.height);
+    path.lineTo(0, size.height / 2);
+    path.quadraticBezierTo(
+        size.width / 2, -size.height / 2, size.width, size.height / 2);
+    path.lineTo(size.width, size.height);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
